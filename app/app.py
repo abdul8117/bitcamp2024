4b4775d2890b67bb3feaabf60b1393498db32c2f
Alex <alexdthi@gmailcom>
2024-04-19 23:07:55 -0400

    commit test

--- app/app.py
+++ app/app.py
@@ -1,7 +1,19 @@
+import random
+
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello from Flask!'
+
+
+@app.route('/rand')
+def get_random_number():
+    return str(random.randint(0, 100))
+
+
+if __name__ == '__main__':
+    app.run()
