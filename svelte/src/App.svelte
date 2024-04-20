909d70d0dc33943b99d37b4003684880afbd8b9e
Zenza235 <paoloatienza235@gmail.com>
2024-04-20 16:40:17 -0400

    fix issue with initMap interferring with map loading

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -20,52 +20,46 @@
 	let end_year_date = '';
 	let k=name.toUpperCase();
 
-	function sendRequest(){
-		// retrive strings
-		// send strings to flask backend
-		// receive json data from backend
-		// load as google maps pins
-		if (start_year_date==null||end_year_date==null){
-			
-		}
-	}
+	// function sendRequest(){
+	// 	// retrive strings
+	// 	// send strings to flask backend
+	// 	// receive json data from backend
+	// 	// load as google maps pins
+	// }
 </script>
 
 <svelte:head>
 	<!-- TODO: fix loading key from backend -->
-	<!-- { #if googleMapsReady } -->
-	<script defer async
-		src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAGSkMd2TxcUsrY3ZTcEESTXKk15uwbklg&callback=initMap">
-	</script>
-	<!-- { /if} -->
-</svelte:head>	
-	<div id="parent" style="background-color: none; display:flex; justify-content: center; height:6%; border:none">
-		<input bind:value={state} placeholder="state" />
-		<input bind:value={county} placeholder="county" />
-		<input bind:value={start_year_date} placeholder="start-year/date" />
-		<input bind:value={end_year_date} placeholder="end-year/date" />
-		<div id = "search button" style="background-color:none; display:flex; justify-content: reverse-end; height:80%" >              
-			<button disabled={!(state && start_year_date && end_year_date)} on:click = {giveData}>Search</button>
-			<style>
-				button{
-					background-color: #f69697;
-					border: none;
-					text-align: center;
-					text-decoration: none;
-					display: inline-block;
-					font-size: 16px;
-					margin: 4px 2px;
-					cursor: pointer;
-				}
-			</style>
-		</div>
-    </div>
-<style>
-:global(body) {
-	padding: 0;
-}
-</style>
+	<script defer async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAGSkMd2TxcUsrY3ZTcEESTXKk15uwbklg&callback=initMap"></script>
+</svelte:head>
 
+<style>
+	:global(body) {
+		padding: 0;
+	}
+	</style>
+
+<div id="parent" style="background-color: none; display:flex; justify-content: center; height:6%; border:none">
+	<input bind:value={state} placeholder="state" />
+	<input bind:value={county} placeholder="county" />
+	<input bind:value={start_year_date} placeholder="start-year/date" />
+	<input bind:value={end_year_date} placeholder="end-year/date" />
+	<div id = "search button" style="background-color:none; display:flex; justify-content: reverse-end; height:80%" >              
+		<button disabled={!(state && start_year_date && end_year_date)}>Search</button>
+		<style>
+			button{
+				background-color: #f69697;
+				border: none;
+				text-align: center;
+				text-decoration: none;
+				display: inline-block;
+				font-size: 16px;
+				margin: 4px 2px;
+				cursor: pointer;
+			}
+		</style>
+	</div>
+</div>
 { #if ready }
 <Map></Map>
 { /if }
